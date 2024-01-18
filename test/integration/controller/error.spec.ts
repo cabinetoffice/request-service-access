aaf40bec8ccd17d74bce835fd55b37a99ccb27a8
mel-am <122634403+mel-am@users.noreply.github.com>
2024-01-18 13:05:59 +0000

    removes info

--- test/integration/controller/error.spec.ts
+++ test/integration/controller/error.spec.ts
@@ -1,4 +1,4 @@
-jest.mock('../../../src/controller/info.controller');
+jest.mock('../../../src/controller/confirmation.controller');
 jest.mock('../../../src/utils/logger');
 
 import { jest, beforeEach, describe, expect, test } from '@jest/globals';
@@ -6,7 +6,7 @@ import request from 'supertest';
 
 import app from '../../../src/app';
 import * as config from '../../../src/config';
-import * as infoController from '../../../src/controller/info.controller';
+import * as confirmationController from '../../../src/controller/confirmation.controller';
 import {
     MOCK_NOT_FOUND_RESPONSE,
     MOCK_ERROR_MESSAGE,
@@ -14,7 +14,7 @@ import {
     MOCK_WRONG_URL
 } from '../../mock/text.mock';
 
-const mockGet = infoController.get as jest.Mock;
+const mockGet = confirmationController.get as jest.Mock;
 
 describe('Error integration tests', () => {
     beforeEach(() => {
@@ -32,7 +32,7 @@ describe('Error integration tests', () => {
         mockGet.mockImplementationOnce(() => {
             throw new Error(MOCK_ERROR_MESSAGE);
         });
-        const res = await request(app).get(config.LANDING_URL);
+        const res = await request(app).get(config.CONFIRMATION_URL);
 
         expect(res.status).toEqual(500);
         expect(res.text).toContain(MOCK_SERVICE_UNAVAILABLE);
