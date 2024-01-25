49aef14118dd56bdacc058451d8ed28d1f63a353
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-01-25 15:56:12 +0000

    add auth middleware to confirmation page and update integration tests

--- src/routes/confirmation.ts
+++ src/routes/confirmation.ts
@@ -3,8 +3,10 @@ import { Router } from 'express';
 import { get } from '../controller/confirmation.controller';
 import * as config from '../config';
 
+import { authentication } from '../middleware/authentication.middleware';
+
 const confirmationRouter = Router();
 
-confirmationRouter.get(config.CONFIRMATION_URL, get);
+confirmationRouter.get(config.CONFIRMATION_URL, authentication, get);
 
 export default confirmationRouter;
