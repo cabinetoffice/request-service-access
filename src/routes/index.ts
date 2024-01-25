bfbaac01760b5e66b4d89745431f8e68de3973c2
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-01-25 15:35:52 +0000

    add auth middleware to landing page and update integration tests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
 import { logger } from '../middleware/logger.middleware';
+
+import landingPageRouter from './landing-page';
 import healthcheckRouter from './healthcheck';
 import confirmationRouter from './confirmation';
-import landingPageRouter from './landing-page';
 
 const router = Router();
 
+// Mounting Logging middleware on all routes
 router.use(logger);
-router.use(healthcheckRouter);
-router.use(confirmationRouter);
+
+// Routes
 router.use(landingPageRouter);
+router.use(confirmationRouter);
+router.use(healthcheckRouter);
 
 export default router;
--- src/routes/landing-page.ts
+++ src/routes/landing-page.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import { authentication } from '../middleware/authentication.middleware';
+
 import { get } from '../controller/landing-page.controller';
 import * as config from '../config';
 
 const landingPageRouter = Router();
 
-landingPageRouter.get(config.LANDING_URL, get);
+landingPageRouter.get(config.LANDING_URL, authentication, get);
 
 export default landingPageRouter;
