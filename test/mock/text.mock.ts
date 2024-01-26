cbfabaa4805e1eccf8e396ed42fdd58e8f8c1cde
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2024-01-26 16:01:25 +0000

    add integration tests

--- test/mock/text.mock.ts
+++ test/mock/text.mock.ts
@@ -1,8 +1,14 @@
 export const MOCK_OK_RESPONSE = 'OK';
 export const MOCK_GET_CONFIRMATION_RESPONSE = 'GitHub request complete';
 export const MOCK_GET_LANDING_RESPONSE = 'GitHub request application';
+
+export const MOCK_GET_REMOVE_MEMBER_RESPONSE = 'Remove a GitHub Member';
+export const MOCK_POST_REMOVE_MEMBER_RESPONSE = 'Github Handle: example, More Details: details';
+
 export const MOCK_NOT_FOUND_RESPONSE = 'Page not found';
 export const MOCK_SERVICE_UNAVAILABLE = 'Sorry, there is a problem with the service';
 export const MOCK_SERVER_ERROR = 'Pipe 3000 requires elevated privileges';
 export const MOCK_ERROR_MESSAGE = 'Something has went wrong';
 export const MOCK_WRONG_URL = '/infooo';
+export const MOCK_REDIRECT_MESSSAGE = 'Found. Redirecting to landing-page';
+
