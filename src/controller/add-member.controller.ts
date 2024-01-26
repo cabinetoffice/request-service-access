6c2a38d456b6a8e74cfa34ddda8448f2494251fd
mel-am <122634403+mel-am@users.noreply.github.com>
2024-01-26 12:10:46 +0000

    Add post

--- src/controller/add-member.controller.ts
+++ src/controller/add-member.controller.ts
@@ -4,3 +4,6 @@ import * as config from '../config';
 export const get = (_req: Request, res: Response) => {
     return res.render(config.ADD_MEMBER);
 };
+export const post = (_req: Request, res: Response) => {
+    return res.render('post request test');
+};
--- src/routes/add-member.ts
+++ src/routes/add-member.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { get } from '../controller/add-member.controller';
+import { get, post } from '../controller/add-member.controller';
 import * as config from '../config';
 
 const addMemberRouter = Router();
 
 addMemberRouter.get(config.ADD_MEMBER_URL, get);
+addMemberRouter.post(config.ADD_MEMBER_URL, post);
 
 export default addMemberRouter;
