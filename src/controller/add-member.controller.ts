cd2d74dec14940e8b7f96fa18b65d049ae344b23
mel-am <122634403+mel-am@users.noreply.github.com>
2024-02-01 12:59:51 +0000

    Update add-member.controller

--- src/controller/add-member.controller.ts
+++ src/controller/add-member.controller.ts
@@ -1,9 +1,31 @@
+// import { Request, Response } from 'express';
+// import * as config from '../config';
+
+// export const get = (_req: Request, res: Response) => {
+//     return res.render(config.ADD_MEMBER);
+// };
+// // export const post = (_req: Request, res: Response) => {
+// //     return res.render('post request test');
+// // };
 import { Request, Response } from 'express';
+import { log } from '../utils/logger';
 import * as config from '../config';
 
 export const get = (_req: Request, res: Response) => {
     return res.render(config.ADD_MEMBER);
 };
-// export const post = (_req: Request, res: Response) => {
-//     return res.render('post request test');
-// };
+
+export const post = (req: Request, res: Response) => {
+
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const gitHubHandle = req.body.gitHubHandle;
+    const emailAddress = req.body.emailAddress;
+    const description = req.body.description;
+
+    // validation middleware and data assignment to be implemented
+
+    log.info(`firstName: ${firstName} lastName: ${lastName}, Github Handle: ${gitHubHandle}, emailAddress: ${emailAddress}, description : ${description} `);
+
+    return res.redirect(config.LANDING);
+};
