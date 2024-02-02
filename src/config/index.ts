5f5e95426e8f3d053981f8d040a74fd9b24f589b
mel-am <122634403+mel-am@users.noreply.github.com>
2024-02-02 12:56:11 +0000

    Revert "Updated index"
    This reverts commit 5333580494153e32ae5c9f75bd01dceff04e7695.

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,11 +11,11 @@ export const PATH_SSL_CERTIFICATE = process.env['PATH_SSL_CERTIFICATE'] || '';
 export const SERVICE_NAME = 'GitHub Requests Application ';
 
 // Template
-export const LANDING = 'landing-page';
 export const NOT_FOUND = 'page-not-found';
 export const NOT_AVAILABLE = 'not-available';
 export const ERROR_PAGE = 'error';
 export const CONFIRMATION = 'confirmation';
+export const LANDING = 'landing-page';
 export const ADD_MEMBER = 'add-member';
 export const REMOVE_MEMBER = 'remove-member';
 
@@ -23,6 +23,7 @@ export const REMOVE_MEMBER = 'remove-member';
 
 export const HEALTHCHECK_URL = '/healthcheck';
 export const CONFIRMATION_URL = '/confirmation';
+export const LANDING_URL = '/landing-page';
 export const ADD_MEMBER_URL = '/add-member';
 export const REMOVE_MEMBER_URL = '/remove-member';
 
