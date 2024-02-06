c6a13a162fffde4d6d2a0fe6059cd0eea7d99984
mel-am <122634403+mel-am@users.noreply.github.com>
2024-02-06 13:16:54 +0000

    Update import

--- test/integration/routes/team-request.spec.ts
+++ test/integration/routes/team-request.spec.ts
@@ -12,7 +12,7 @@ import { logger } from '../../../src/middleware/logger.middleware';
 import { log } from '../../../src/utils/logger';
 import { authentication } from '../../../src/middleware/authentication.middleware';
 
-import { MOCK_REDIRECT_MESSAGE as MOCK_REDIRECT_MESSAGE, MOCK_GET_TEAM_REQUEST_RESPONSE, MOCK_POST_TEAM_REQUEST_RESPONSE } from '../../mock/text.mock';
+import { MOCK_REDIRECT_MESSAGE, MOCK_GET_TEAM_REQUEST_RESPONSE, MOCK_POST_TEAM_REQUEST_RESPONSE } from '../../mock/text.mock';
 import { MOCK_POST_TEAM_REQUEST } from '../../mock/data';
 
 const mockedLogger = logger as jest.Mock<typeof logger>;
