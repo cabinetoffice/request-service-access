2b6d06ef5b6bee740ebe6e203961b8a20fcb7c9c
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2024-02-07 12:40:21 +0000

    add member-request controller and route tests

---
+++ test/unit/controller/member-request.controller.spec.ts
@@ -0,0 +1,60 @@
+jest.mock('../../../src/utils/logger');
+
+import { describe, expect, afterEach, test, jest } from '@jest/globals';
+import { Request, Response } from 'express';
+
+import { get, post } from '../../../src/controller/member-request.controller';
+import * as config from '../../../src/config';
+import { log } from '../../../src/utils/logger';
+
+import { MOCK_POST_MEMBER_REQUEST } from '../../mock/data';
+import { MOCK_POST_MEMBER_REQUEST_RESPONSE } from '../../mock/text.mock';
+
+const req = {
+    body: MOCK_POST_MEMBER_REQUEST
+} as Request;
+
+const mockResponse = () => {
+    const res = {} as Response;
+    res.render = jest.fn().mockReturnValue(res) as any;
+    res.redirect = jest.fn().mockReturnValue(res) as any;
+    return res;
+};
+
+describe('Member-request controller test suites', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    describe('member-request GET tests', () => {
+
+        test('should render member-request page', () => {
+            const res = mockResponse();
+
+            get(req, res);
+
+            expect(res.render).toHaveBeenCalledWith(config.MEMBER_REQUST);
+        });
+    });
+
+    describe('member-request POST tests', () => {
+
+        test('should redirect to landing-page on POST request', () => {
+            const res = mockResponse();
+
+            post(req, res);
+
+            expect(res.redirect).toBeCalledWith(config.LANDING);
+        });
+        test('Should log the github handle on POST request.', () => {
+            const res = mockResponse();
+
+            const mockLogInfo = log.info as jest.Mock;
+
+            post(req, res);
+
+            expect(mockLogInfo).toHaveBeenCalledWith(MOCK_POST_MEMBER_REQUEST_RESPONSE);
+
+        });
+    });
+});
