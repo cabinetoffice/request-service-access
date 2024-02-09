9542951d2c270a8e0f9df72636a597beb7ad2241
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-02-09 13:53:16 +0000

    Extract description validation into a shared field

--- src/validation/remove-member.validation.ts
+++ src/validation/remove-member.validation.ts
@@ -1,8 +1,9 @@
 import { body } from 'express-validator';
 
 import { ErrorMessages } from './error.messages';
+import { descriptionValidation } from './fields/description.validation';
 
 export const removeMember = [
     body('github_handle').not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.GIT_HANDLE),
-    body('description').isLength({ max: 1000 }).withMessage(ErrorMessages.DESCRIPTION_LENGTH)
+    ...descriptionValidation
 ];
