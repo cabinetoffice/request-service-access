d1163461aac07375619f47020d294af793a88ca9
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-02-14 15:07:18 +0000

    add validation to repo-request page

--- src/routes/repo-request.ts
+++ src/routes/repo-request.ts
@@ -4,10 +4,12 @@ import { authentication } from '../middleware/authentication.middleware';
 
 import { get, post } from '../controller/repo-request.controller';
 import * as config from '../config';
+import { repoRequest } from '../validation/repo-request.validation';
+import { checkValidations } from '../middleware/validation.middleware';
 
 const addRepoRouter = Router();
 
 addRepoRouter.get(config.REPO_REQUEST_URL, authentication, get);
-addRepoRouter.post(config.REPO_REQUEST_URL, authentication, post);
+addRepoRouter.post(config.REPO_REQUEST_URL, authentication, ...repoRequest, checkValidations, post);
 
 export default addRepoRouter;
