abfbe9453ae2854e6393d7eb1b7031b53e0c7a3a
mel-am <122634403+mel-am@users.noreply.github.com>
2024-02-20 17:04:17 +0000

    Add validation to add team-request

--- src/validation/team-request.validation.ts
+++ src/validation/team-request.validation.ts
@@ -1,6 +1,11 @@
+import { body } from 'express-validator';
+import { ErrorMessages } from './error.messages';
 import { descriptionValidation } from './fields/description.validation';
 import { teamNameValidation } from './fields/team-name.validation';
 
 export const teamRequest = [
-    ...teamNameValidation, ...descriptionValidation
+    ...teamNameValidation,
+    body('team_name').not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.TEAM_NAME),
+    ...descriptionValidation,
+    body('description').not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.DESCRIPTION)
 ];
