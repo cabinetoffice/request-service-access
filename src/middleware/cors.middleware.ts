f462ab93ff1d2517918c571435ef39ce2f763471
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2024-02-20 13:23:45 +0000

    update cors middleware

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -1,12 +1,11 @@
-import { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 
 import * as config from '../config';
 
-export const setCors = (_req: Request, _res: Response, next: NextFunction) => {
-    cors({
+export const configureCors = (app: express.Application) => {
+    app.use(cors({
         origin: [config.CDN_HOST, config.BASE_URL],
         credentials: true
-    });
-    next();
+    }));
 };
