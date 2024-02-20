3b9ec9ac3f656b81dd10eb18f328bc8e54836190
mel-am <122634403+mel-am@users.noreply.github.com>
2024-02-20 17:09:53 +0000

    Update repo-request validation

--- src/validation/repo-request.validation.ts
+++ src/validation/repo-request.validation.ts
@@ -1,6 +1,11 @@
+import { body } from 'express-validator';
+import { ErrorMessages } from './error.messages';
 import { descriptionValidation } from './fields/description.validation';
 import { repoNameValidation } from './fields/repo-name.validation';
 
 export const repoRequest = [
-    ...repoNameValidation, ...descriptionValidation
+    ...repoNameValidation,
+    body('repo_name').not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.TEAM_NAME),
+    ...descriptionValidation,
+    body('description').not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.DESCRIPTION)
 ];
