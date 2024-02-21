55c5ad89098d4eda1958831338eb298fbe120387
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-02-21 14:28:01 +0000

    depersonalise git handle error message

--- src/validation/error.messages.ts
+++ src/validation/error.messages.ts
@@ -1,5 +1,5 @@
 export enum ErrorMessages {
-  GIT_HANDLE = 'Enter the username of your GitHub handle (aka GitHub account)',
+  GIT_HANDLE = 'Enter the username of the GitHub handle (aka GitHub account)',
   DESCRIPTION_LENGTH = 'Description must be 1000 characters or less',
   FIRST_NAME = 'Enter the first name of the GitHub account holder',
   LAST_NAME = 'Enter the last name of the GitHub account holder',
