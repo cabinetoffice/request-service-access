55048a166c159cc84b1920bdb9b45667f3c606bd
Mouhajer-CO <mouhajer.zaouali@digital.cabinet-office.gov.uk>
2024-02-29 09:29:05 +0000

    Add session storage to add team page and update unit tests

--- src/controller/add-team.controller.ts
+++ src/controller/add-team.controller.ts
@@ -1,19 +1,30 @@
-import { Request, Response } from 'express';
-import { log } from '../utils/logger';
+import { NextFunction, Request, Response } from 'express';
+import { getSessionData, setSessionData } from '@co-digital/login';
+
 import * as config from '../config';
+import { log } from '../utils/logger';
+
+import { AddTeamKey } from '../model/add-team.model';
 
 export const get = (_req: Request, res: Response) => {
     return res.render(config.ADD_TEAM);
 };
 
-export const post = (req: Request, res: Response) => {
+export const post = (req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const teamName = req.body.team_name;
+        const githubHandle = req.body.github_handle;
 
-    const teamName = req.body.team_name;
-    const githubHandle = req.body.github_handle;
+        log.info(`Team Name: ${teamName}, Team Maintainer GitHub Handle: ${githubHandle}`);
 
-    // validation middleware and data assignment to be implemented
+        setSessionData(req.session, {
+            ...getSessionData(req.session),
+            [AddTeamKey]: { ...req.body }
+        });
 
-    log.info(`Team Name: ${teamName}, Team Maintainer GitHub Handle: ${githubHandle}`);
-
-    return res.redirect(config.HOME);
+        return res.redirect(config.HOME);
+    } catch (err: any) {
+        log.errorRequest(req, err.message);
+        next(err);
+    }
 };
