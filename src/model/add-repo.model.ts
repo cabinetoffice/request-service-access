cc9eff37e5a5d3de0004aec9bed17f76f11aa826
Mouhajer-CO <mouhajer.zaouali@digital.cabinet-office.gov.uk>
2024-03-05 14:05:43 +0000

    Update MVC for add-repo page

--- src/model/add-repo.model.ts
+++ src/model/add-repo.model.ts
@@ -1,12 +1,14 @@
 export const AddRepoKey = 'add_repo';
 
 export const AddRepoMappingKeys: (keyof AddRepo)[] = [
+    'id',
     'repo_name',
     'visibility',
     'description'
 ];
 
 export interface AddRepo {
+  id?: string;
   repo_name?: string;
   visibility?: string;
   description?: string;
--- src/routes/add-repo.ts
+++ src/routes/add-repo.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 
-import { authentication } from '../middleware/authentication.middleware';
-
-import { get, post } from '../controller/add-repo.controller';
 import * as config from '../config';
-import { addRepo } from '../validation/add-repo.validation';
+
 import { checkValidations } from '../middleware/validation.middleware';
+import { authentication } from '../middleware/authentication.middleware';
+import { addRepo as addRepoValidation } from '../validation/add-repo.validation';
+import { get, getById, post, postById, removeById } from '../controller/add-repo.controller';
 
 const addRepoRouter = Router();
 
 addRepoRouter.get(config.ADD_REPO_URL, authentication, get);
-addRepoRouter.post(config.ADD_REPO_URL, authentication, ...addRepo, checkValidations, post);
+addRepoRouter.post(config.ADD_REPO_URL, authentication, ...addRepoValidation, checkValidations, post);
+addRepoRouter.get(config.ADD_REPO_URL + config.PARAM_ID, authentication, getById);
+addRepoRouter.get(config.ADD_REPO_URL + config.REMOVE + config.PARAM_ID, authentication, removeById);
+addRepoRouter.post(config.ADD_REPO_URL + config.PARAM_ID, authentication, ...addRepoValidation, checkValidations, postById);
 
 export default addRepoRouter;
