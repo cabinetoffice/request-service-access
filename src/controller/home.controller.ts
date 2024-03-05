09e942759cd90891d4eceb7bcdcbba58314c1b22
Mouhajer-CO <mouhajer.zaouali@digital.cabinet-office.gov.uk>
2024-03-05 14:05:17 +0000

    Update configuration, home controller, middleware and model

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -1,6 +1,19 @@
-import { Request, Response } from 'express';
-import * as config from '../config';
+import { NextFunction, Request, Response } from 'express';
+import { getSessionData } from '@co-digital/login';
 
-export const get = (_req: Request, res: Response) => {
-    return res.render(config.HOME);
+import * as config from '../config';
+import { log } from '../utils/logger';
+import { ApplicationData } from '../model/application.model';
+
+export const get = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const appData: ApplicationData = getSessionData(req.session);
+
+        return res.render(config.HOME, {
+            ...appData
+        });
+    } catch (err: any) {
+        log.errorRequest(req, err.message);
+        next(err);
+    }
 };
