d038a08ddcd77b54cf5bef1b1c0ed98a47746778
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-03-28 10:45:08 +0000

    add missing authentication mock on cookies page integration test

--- test/integration/routes/cookies.spec.ts
+++ test/integration/routes/cookies.spec.ts
@@ -1,4 +1,5 @@
 jest.mock('../../../src/middleware/logger.middleware');
+jest.mock('../../../src/middleware/authentication.middleware');
 jest.mock('../../../src/utils/logger');
 
 import { jest, beforeEach, describe, expect, test } from '@jest/globals';
@@ -8,11 +9,14 @@ import request from 'supertest';
 import app from '../../../src/app';
 import * as config from '../../../src/config';
 import { logger } from '../../../src/middleware/logger.middleware';
+import { authentication } from '../../../src/middleware/authentication.middleware';
 
 import { MOCK_COOKIES_RESPONSE } from '../../mock/text.mock';
 
 const mockedLogger = logger as jest.Mock<typeof logger>;
 mockedLogger.mockImplementation((_req: Request, _res: Response, next: NextFunction) => next());
+const mockedAuth = authentication as jest.Mock<typeof authentication>;
+mockedAuth.mockImplementation((_req: Request, _res: Response, next: NextFunction) => next());
 
 describe('Cookies endpoint integration tests', () => {
     beforeEach(() => {
@@ -25,6 +29,7 @@ describe('Cookies endpoint integration tests', () => {
 
             expect(res.text).toContain(MOCK_COOKIES_RESPONSE);
             expect(mockedLogger).toHaveBeenCalledTimes(1);
+            expect(mockedAuth).toHaveBeenCalledTimes(1);
         });
     });
 });
