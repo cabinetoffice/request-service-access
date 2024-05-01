605b7045c9f58cffbbfc66e3b3ff1434089f86c5
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-05-01 14:32:50 +0100

    Simplify getPreviousPageUrl util to only redirect to check-your-requests if the previousPage query param is present

--- src/utils/getPreviousPageUrl.ts
+++ src/utils/getPreviousPageUrl.ts
@@ -1,17 +1,4 @@
 import { Request } from 'express';
 import * as config from '../config';
 
-export const getPreviousPageUrl = (req: Request): string => {
-
-    const previousPage = req.query?.[config.PREVIOUS_PAGE_QUERY_PARAM];
-
-    switch (previousPage) {
-        case config.HOME_URL:
-            return config.HOME_URL;
-        case config.CHECK_YOUR_REQUESTS_URL:
-            return config.CHECK_YOUR_REQUESTS_URL;
-        default:
-            return config.HOME_URL;
-    }
-
-};
+export const getPreviousPageUrl = (req: Request): string => (config.PREVIOUS_PAGE_QUERY_PARAM in req.query) ? config.CHECK_YOUR_REQUESTS_URL : config.HOME_URL;
--- test/unit/utils/getPreviousPageUrl.spec.ts
+++ test/unit/utils/getPreviousPageUrl.spec.ts
@@ -12,43 +12,23 @@ describe('getPreviousPage test suite', () => {
         jest.resetAllMocks();
     });
 
-    test('Should return home page URL if the previousPage query param value is /home', () => {
+    test('Should return home page URL if the previousPage query param is not present', () => {
 
-        const mockRequestWithHomeQueryParam = { ...mockRequest(), query: { previousPage: config.HOME_URL } } as unknown as Request;
+        const mockRequestWithNoQueryParam = { ...mockRequest(), query: {} } as unknown as Request;
 
-        const previousPageUrl = getPreviousPageUrl(mockRequestWithHomeQueryParam);
+        const previousPageUrl = getPreviousPageUrl(mockRequestWithNoQueryParam);
 
         expect(previousPageUrl).toBe(config.HOME_URL);
 
     });
 
-    test('Should return check-your-answers page URL if the previousPage query param value is /check-your-answers', () => {
+    test('Should return check-your-answers page URL if the previousPage query param is present', () => {
 
-        const mockRequestWithCheckYourAnswersQueryParam = { ...mockRequest(), query: { previousPage: config.CHECK_YOUR_REQUESTS_URL } } as unknown as Request;
+        const mockRequestWithQueryParam = { ...mockRequest(), query: { previousPage: config.PREVIOUS_PAGE_QUERY_PARAM } } as unknown as Request;
 
-        const previousPageUrl = getPreviousPageUrl(mockRequestWithCheckYourAnswersQueryParam);
+        const previousPageUrl = getPreviousPageUrl(mockRequestWithQueryParam);
 
         expect(previousPageUrl).toBe(config.CHECK_YOUR_REQUESTS_URL);
-
-    });
-
-    test('Should return home page URL if the previousPage query param is undefined', () => {
-
-        const mockRequestWithNoQueryParams = mockRequest({});
-
-        const previousPageUrl = getPreviousPageUrl(mockRequestWithNoQueryParams);
-
-        expect(previousPageUrl).toBe(config.HOME_URL);
-
-    });
-
-    test('Should return home page URL if the previousPage query param is another page', () => {
-
-        const mockRequestWithOtherPage = { ...mockRequest(), query: { previousPage: '/other-page' } } as unknown as Request;
-
-        const previousPageUrl = getPreviousPageUrl(mockRequestWithOtherPage);
-
-        expect(previousPageUrl).toBe(config.HOME_URL);
 
     });
 });
