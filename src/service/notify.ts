6d9e5b60c7e4d03d7283e7557cd5bff5a1b465f1
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2024-05-14 17:33:28 +0100

    add try catch logic to confirmationEmail function

--- src/service/notify.ts
+++ src/service/notify.ts
@@ -4,17 +4,21 @@ import { isFeatureEnabled } from '../utils/isFeatureEnabled';
 import * as config from '../config';
 
 export const confirmationEmail = async (emailAddress: string, reference_number: string): Promise<void> => {
-    const notifyClient = new NotifyClient(config.NOTIFY_API_KEY);
-    if (isFeatureEnabled(config.FEATURE_FLAG_NOTIFY)) {
-        log.info('Email confirmation is enabled.');
-        await notifyClient.sendEmail(config.NOTIFY_EMAIL_TEMPLATE, emailAddress, {
-            personalisation: {
-                'reference_number': reference_number,
-                'team_email': config.NOTIFY_TEAM_EMAIL
-            },
-            reference: undefined,
-        });
-    } else {
-        log.info('Email confirmation is disabled.');
+    try {
+        if (isFeatureEnabled(config.FEATURE_FLAG_NOTIFY)) {
+            const notifyClient = new NotifyClient(config.NOTIFY_API_KEY);
+            log.info('Email confirmation is enabled.');
+            await notifyClient.sendEmail(config.NOTIFY_EMAIL_TEMPLATE, emailAddress, {
+                personalisation: {
+                    'reference_number': reference_number,
+                    'team_email': config.NOTIFY_TEAM_EMAIL
+                },
+                reference: undefined,
+            });
+        } else {
+            log.info('Email confirmation is disabled.');
+        }
+    } catch (err: any){
+        log.error(`Error: ${err.message}`);
     }
 };
