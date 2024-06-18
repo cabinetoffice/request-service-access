1e78256f179822a6dc7164fa6232f7a111d47b41
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-06-18 17:28:35 +0100

    mount addUserEmail middleware on check-your-requests post route

--- src/routes/check-your-requests.ts
+++ src/routes/check-your-requests.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { authentication } from '../middleware/authentication.middleware';
+import { addUserEmail } from '../middleware/addUserEmail.middleware';
 
 import { get, post } from '../controller/check-your-requests.controller';
 import * as config from '../config';
@@ -8,6 +9,6 @@ import * as config from '../config';
 const checkYourRequestsRouter = Router();
 
 checkYourRequestsRouter.get(config.CHECK_YOUR_REQUESTS_URL, authentication, get);
-checkYourRequestsRouter.post(config.CHECK_YOUR_REQUESTS_URL, authentication, post);
+checkYourRequestsRouter.post(config.CHECK_YOUR_REQUESTS_URL, authentication, addUserEmail, post);
 
 export default checkYourRequestsRouter;
